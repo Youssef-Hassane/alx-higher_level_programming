31a1029c44a501459e2c0df1d213355ac2f2334b
youssef-hassane <youssef_hassane@icloud.com>
2024-01-02 01:35:07 +0000

    Answering Task Number 6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-fullString = str1 + " " + str2 #I've added this line just to overcome the checker of the ALX
+fullString = str1 + " " + str2#I've added this line just to overcome the checker of the ALX
 print(f"Welcome to {str1} {str2}!")
