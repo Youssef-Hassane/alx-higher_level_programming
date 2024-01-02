d58ec70a02050b7a3dda10f6119898c0378660b8
youssef-hassane <youssef_hassane@icloud.com>
2024-01-02 01:40:57 +0000

    Answering Task Number 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-
 wordFirstThree = word[:3]
 wordLastTwo = word[-2:]
 middleWord = word[1:-1]
-
 print(f"First 3 letters: {wordFirstThree}")
 print(f"Last 2 letters: {wordLastTwo}")
 print(f"Middle word: {middleWord}")
