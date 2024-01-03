a381821f0ab9c0ad3f4d6a32f90493cb9cd3b619
youssef-hassane <youssef_hassane@icloud.com>
2024-01-03 01:38:05 +0000

    Answering Task Number 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for num in range(100):
-    print("{:02d}".format(num), end=(", " if num < 99 else "\n"))
+for theNumber in range(100):
+    print("{:02d}".format(theNumber), end=(", " if theNumber < 99 else "\n"))
