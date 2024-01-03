c1ec2277529a5852e52b4354d8c839883c4b9892
youssef-hassane <youssef_hassane@icloud.com>
2024-01-03 01:01:55 +0000

    test

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,7 +3,7 @@ def remove_char_at(str, n):
     if n < 0:
         return (str)
     return (str[:n] + str[n+1:])
-#!/usr/bin/python3
+
 def magic_calculation(a, b, c):
     if a < b:
         return (c)
