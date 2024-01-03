e7e0f483e8239f082db30506238771100e15279a
youssef-hassane <youssef_hassane@icloud.com>
2024-01-03 01:27:45 +0000

    Recover the original files

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(i), end='')
+
+for char in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(char)), end="")
