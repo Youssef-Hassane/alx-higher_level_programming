9e8a4b6cb28fbff14db7ef87564dd66d35540460
youssef-hassane <youssef_hassane@icloud.com>
2024-01-03 01:40:16 +0000

    Answering Task Number 7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 
 for index_1 in range(10):
     for index_2 in range(index_1 + 1, 10):
-        print("{:d}{:d}".format(index_1, index_2), end=(", " if index_1 < 8 or index_2 < 9 else "\n"))
+        print("{:d}{:d}".format(index_1, index_2),
+              end=(", " if index_1 < 8 or index_2 < 9 else "\n"))
