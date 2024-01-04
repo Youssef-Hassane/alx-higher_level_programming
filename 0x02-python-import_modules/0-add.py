fa164be81ed7e1e872d3f1b21e0168a3207f7533
youssef-hassane <youssef_hassane@icloud.com>
2024-01-04 18:26:05 +0000

    Answering Task Number 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     from add_0 import add
     firstNumber = 1
     secondNumber = 2
     print("{} + {} = {}".format(
-        firstNumber, secondNumber,
-      add(firstNumber, secondNumber)
-    ))
+        firstNumber, secondNumber, add(firstNumber, secondNumber)))
