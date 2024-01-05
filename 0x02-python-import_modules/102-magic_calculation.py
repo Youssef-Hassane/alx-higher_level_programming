4eb02925868fd96ebf89899bfdec1d08a3d2daec
youssef-hassane <youssef_hassane@icloud.com>
2024-01-05 00:55:46 +0000

    Answering Task Number 9

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,8 @@
 def magic_calculation(a, b):
-    add = __import__('magic_calculation_102', globals(), locals(), ['add'], 0).add
-    sub = __import__('magic_calculation_102', globals(), locals(), ['sub'], 0).sub
+    add = __import__('magic_calculation_102', globals(),
+                     locals(), ['add'], 0).add
+    sub = __import__('magic_calculation_102', globals(),
+                     locals(), ['sub'], 0).sub
 
     if a < b:
         c = add(a, b)
