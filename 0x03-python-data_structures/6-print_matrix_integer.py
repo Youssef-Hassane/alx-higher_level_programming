0d8d1b37424aabfbff9d51a2b745d4fa8fe9e8c4
youssef-hassane <youssef_hassane@icloud.com>
2024-01-08 16:53:26 +0200

    Answering task number 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,9 @@ def print_matrix_integer(matrix=[[]]):
         return
 
     for row in matrix:
-        for column in row:
-            print("{:d}".format(column), end=" ")
+        for col_index, col in enumerate(row):
+            print("{:d}".format(col), end="")
+            if col_index < len(row) - 1:
+                print(" ", end="")
         print()
+        
