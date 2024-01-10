b570e198520ae81936bc7ec0a0d7767a1175a513
Youssef-Hassane <youssef_hassane@icloud.com>
2024-01-10 22:54:41 +0200

    Answering task number 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def common_elements(firstSet, secondSet):
+    # First Set: { "Python", "C", "Javascript" }
+    # Seond Set: { "Bash", "C", "Ruby", "Perl" }
+    intersection_set = firstSet & secondSet  #  "C"
+    return intersection_set
+
+# Alternative way to do it:
+# def common_elements(set_1, set_2):
+#     return set_1.intersection(set_2)
