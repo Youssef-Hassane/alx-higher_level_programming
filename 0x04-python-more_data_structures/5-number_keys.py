b1d79f646370c008271987c5664f0a8bd5de942c
Youssef-Hassane <youssef_hassane@icloud.com>
2024-01-11 00:57:15 +0200

    Answering task number 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def number_keys(theDictionary):
+    # Use the len() function to get the number of keys in the dictionary
+    num_keys = len(theDictionary.keys())
+    # Return the number of keys
+    return num_keys
