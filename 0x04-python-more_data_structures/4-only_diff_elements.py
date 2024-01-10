3267b2cdb8bd84b34b398fb7cf3fbb567537e635
Youssef-Hassane <youssef_hassane@icloud.com>
2024-01-11 00:26:37 +0200

    Answering task number 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,5 @@
 def only_diff_elements(firstSet, secondSet):
     # Use symmetric difference to get elements present in only one set
     diff_set = firstSet.symmetric_difference(secondSet)
-    
+    # Return the difference set
     return diff_set
