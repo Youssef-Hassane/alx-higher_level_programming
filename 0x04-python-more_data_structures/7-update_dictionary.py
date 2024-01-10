0f4d465542801f668f462b1254577a70d374de3d
Youssef-Hassane <youssef_hassane@icloud.com>
2024-01-11 01:03:11 +0200

    Answering task number 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(theDictionary, theKey, theValue):
+    # Update or add the key-value pair in the dictionary
+    theDictionary[theKey] = theValue
+    # Return the updated dictionary
+    return theDictionary
