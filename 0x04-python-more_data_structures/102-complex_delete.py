ae5980681aa24baf29933bd9470a6b4f167410d1
Youssef-Hassane <youssef_hassane@icloud.com>
2024-01-11 04:31:51 +0200

    Answering task number 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    # Create a new dictionary
-    new_dict = {}
-    # Iterate through the original dictionary and add 
-    # key-value pairs that don't match the specified value
-    for key, val in a_dictionary.items():
-        # Check if the value matches the specified value
-        if val != value:
-            # Add the key-value pair to the new dictionary
-            new_dict[key] = val
-    # Return the new dictionary
-    return new_dict
+    # Create a list of keys to delete
+    keys_to_delete = [
+        key
+        for key, val in a_dictionary.items()
+        if val == value
+    ]
+
+    # Delete the keys from the dictionary
+    for key in keys_to_delete:
+        del a_dictionary[key]
+
+    return a_dictionary
