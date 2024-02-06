df496f80fe7be6214ffcd181e2f41549b83f4d69
Youssef-hasssane <youssefalmasy@icloud.com>
2024-02-06 04:06:16 +0200

    Answering task number 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """the BaseGeometry class"""
+
+    def area(self):
+        """Raises an exception when called"""
+        raise Exception("area() is not implemented")
