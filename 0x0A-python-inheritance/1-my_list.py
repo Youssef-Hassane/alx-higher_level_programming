630b24e05474d674505c4ca2e7a6a6695eef64ce
Youssef-hasssane <youssefalmasy@icloud.com>
2024-02-06 03:15:48 +0200

    Answering task number 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """Class MyList that inherits from list"""
+    pass
+
+    def print_sorted(self):
+        """Print the list in ascending order"""
+        print(sorted(self))
