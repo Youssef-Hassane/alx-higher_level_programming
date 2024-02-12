99c277657a46e5d560cb8047c6590a0829123ee7
Youssef-hasssane <youssefalmasy@icloud.com>
2024-02-12 23:25:40 +0200

    Answering task number 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,22 +7,16 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square that inherits from Rectangle."""
+    """Square class that inherits from Rectangle that inherits BaseGeometry"""
 
     def __init__(self, size):
-        """Initialize a new Square with a given size.
+        """Method for initialized the attrubutes"""
 
-        Args:
-            size (int): The size of the square sides.
-        """
-        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        """Return the area of the square."""
-        return self.__size ** 2
+        """rectangle area"""
 
-    def __str__(self):
-        """Return the print() and str() representation of the Square."""
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        return self.__size ** 2
