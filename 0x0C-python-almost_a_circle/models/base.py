907800b3212b627079511fbb0db086d430c4eb2c
Youssef-hasssane <youssefalmasy@icloud.com>
2024-02-12 20:03:59 +0200

    Answering the 0x0C. Python - Almost a circle Task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,3 +29,14 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Write the JSON string representation of list_objs to a file """
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                f.write(Base.to_json_string(list_dicts))
