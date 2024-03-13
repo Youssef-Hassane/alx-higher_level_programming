416d4c4b999de9f990f1d3b0763cd8f80a2a64a4
Youssef-Hassane <youssef_hassane@icloud.com>
2024-03-13 05:40:48 +0200

    Task Number 6 - DONE

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- TASK - 6. List valuess
+-- A script that lists all rows in the table first_table from the database hbtn_0c_0
+	-- The database name will be passed as an argument of the mysql command
+	-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+SELECT * FROM first_table;
