f44f0747e2e13fd6578c4cde74e642fcdacc47b0
Youssef-Hassane <youssef_hassane@icloud.com>
2024-03-14 14:31:53 +0200

    Task Number 9 - DONE

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,2 @@
 -- Lists all cities in the database hbtn_0d_usa.
-
-SELECT c.id, c.name, s.name
-    FROM cities AS c
-        INNER JOIN states AS s ON c.state_id = s.id
-        FROM hbtn_0d_usa
-ORDER BY c.id;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id;
