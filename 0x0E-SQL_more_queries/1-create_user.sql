03e18d5d3b51c58595b3aeba3cc523275f19a84a
Youssef-Hassane <youssef_hassane@icloud.com>
2024-03-14 14:29:29 +0200

    Task Number 1 - DONE

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,7 +3,7 @@
 -- Set user_0d_1s password to user_0d_1_pwd
 -- Do not fail if user_0d_1 already exists
 CREATE USER 
-    IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+    IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY `user_0d_1_pwd`;
 GRANT ALL 
     ON *.* 
     TO user_0d_1@localhost;
