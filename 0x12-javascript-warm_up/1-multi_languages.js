d565a8ce43bd088052016b482ab583c85034d0e5
Youssef-Hassane <youssef_hassane@icloud.com>
2024-04-09 04:03:47 +0200

    Task #1 - DONE

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-/* 
-Prints "
-C is fun 
-Python is cool
-JavaScript is amazing" to the console 
-*/
 
-console.log(`C is fun\nPython is cool\nJavascript is amazing`);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
