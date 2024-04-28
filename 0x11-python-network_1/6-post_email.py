903904bba955f8f5518ded1188b617bae2f0abc4
Youssef-Hassane <youssef_hassane@icloud.com>
2024-04-29 00:25:57 +0300

    Python - Network #1 - DONE

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,20 +5,14 @@ A script that retrieves the X-Request-Id header from a URL.
 Usage:
     python 6-post_email.py <url>
 """
+
+
 import sys
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
-    # Get URL from command line argument
     url = sys.argv[1]
-
-    # Create request object
-    request = urllib.request.Request(url)
-
-    # Open URL and print X-Request-Id header
-    with urllib.request.urlopen(request) as response:
-        # Print X-Request-Id header
-        header_dict = dict(response.headers)
-        request_id = header_dict.get("X-Request-Id")
-        print("X-Request-Id:", request_id)
+    values = {"email": sys.argv[2]}
+    with requests.post(sys.argv[1], data=values) as res:
+        print(res.text)
