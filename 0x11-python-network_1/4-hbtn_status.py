d68b274befc752c8e5cb31492b4c4307f8dfdad3
Youssef-Hassane <youssef_hassane@icloud.com>
2024-04-29 00:24:16 +0300

    Python - Network #1 - DONE

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,11 +9,11 @@ import requests
 if __name__ == "__main__":
     # Send a GET request to the URL and store
     # the response in the variable `response`
-    response = requests.get("https://intranet.hbtn.io/status")
-
-    # Print the body response of the `response`
-    print("Body response:")
-    # Print the type of the response text
-    print("\t- type: {}".format(type(response.text)))
-    # Print the content of the response text
-    print("\t- content: {}".format(response.text))
+    with requests.get("https://alx-intranet.hbtn.io/status") as res:
+        r = res.text
+        # Print the body response of the `response`
+        print("Body response:")
+        # Print the type of the response text
+        print(f"\t- type: {type(r)}")
+         # Print the content of the response text
+        print(f"\t- content: {r}")
