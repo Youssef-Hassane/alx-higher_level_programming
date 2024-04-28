ea5e23dcbf6d6060eb133277ada5c8525b82de2a
Youssef-Hassane <youssef_hassane@icloud.com>
2024-04-29 00:22:17 +0300

    Python - Network #1 - DONE

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,14 +5,5 @@ import sys
 import urllib.request
 
 if __name__ == "__main__":
-    # Get URL from command line argument
-    url = sys.argv[1]
-
-    # Create request object
-    request = urllib.request.Request(url)
-
-    # Open URL and print X-Request-Id header
-    with urllib.request.urlopen(request) as response:
-        header_dict = dict(response.headers)
-        request_id = header_dict.get("X-Request-Id")
-        print("X-Request-Id:", request_id)
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.headers["X-Request-Id"])
