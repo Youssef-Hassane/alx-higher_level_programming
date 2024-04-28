efa59452e991b128003ae14a0f5d545c93d1e18f
Youssef-Hassane <youssef_hassane@icloud.com>
2024-04-29 00:27:48 +0300

    Python - Network #1 - DONE

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
+"""lists the 10 most recent commits on a given GitHub repository.
 """
-Lists the 10 most recent commits on a given GitHub repository.
-"""
-
 import sys
 import requests
 
 
-# Main function of the program
 if __name__ == "__main__":
-    # Get GitHub credentials from command line arguments
-    owner: str = sys.argv[1]  # Owner of the repository
-    repository: str = sys.argv[2]  # Name of the repository
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    # Create URL for request
-    url: str = "https://api.github.com/repos/{}/{}/commits"\
-        .format(owner, repository)
-
-    # Send GET request to GitHub API to retrieve commits
-    response: requests.Response = requests.get(url)
-
-    # Parse JSON response
-    commits: list = response.json()
-
-    # Print the 10 most recent commits
-    for i in range(min(len(commits), 10)):
-        sha: str = commits[i].get("sha")  # Get commit SHA
-        # Get author name
-        author_name: str = commits[i].get("commit").get("author").get("name")
-        print("{}: {}".format(sha, author_name))  # Print commit info
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
