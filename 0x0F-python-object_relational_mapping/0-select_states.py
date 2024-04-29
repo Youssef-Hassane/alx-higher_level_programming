711566b73957e8d393c3fc4e13ddd3ccddaaa6b4
Youssef-Hassane <youssef_hassane@icloud.com>
2024-04-30 01:59:49 +0300

    Task #1 - DONE

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """a script that lists all states from the database hbtn_0e_0_usa"""
-
 import MySQLdb
 import sys
 
@@ -8,7 +7,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ")
+    cursor.execute("SELECT * FROM states")
     results = cursor.fetchall()
     for row in results:
         print(row)
+    cursor.close()
+    db.close()
