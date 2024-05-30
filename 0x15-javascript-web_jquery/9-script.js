96cfd71576b914eeafb07d9eec331c2c94a721e8
Youssef-Hassane <youssef_hassane@icloud.com>
2024-05-30 04:30:19 +0300

    Task #9 - DONE

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -4,6 +4,7 @@ const url = "https://hellosalut.stefanbohacek.dev/?lang=fr";
 /* Get data from url */
 $.getJSON(url, callBackFunction);
 
+/* Callback function to get and set data */
 function callBackFunction(data) {
 	const textString = data.hello;
 	$("#hello").text(textString);
